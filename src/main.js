718402f758e0d7c5858e55889474d623d5a5146d
Kae <kaehehehe@gmail.com>
2022-02-12 19:54:18 +0900

    :sparkles: youtube API로 데이터 불러오기

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ const detailedHumbergerMenu = document.querySelector(
   '.material-icons.humberger-menu'
 );
 const detailedNav = document.querySelector('.detailed-nav');
+const main = document.querySelector('.main');
+const videos = document.querySelector('.videos');
 
 defaultHumbergerMenu.addEventListener('click', () => {
   detailedNav.classList.add('show');
@@ -10,4 +12,33 @@ defaultHumbergerMenu.addEventListener('click', () => {
 
 detailedHumbergerMenu.addEventListener('click', () => {
   detailedNav.classList.remove('show');
+});
+
+window.addEventListener('load', () => {
+  const requestOptions = {
+    method: 'GET',
+    redirect: 'follow',
+  };
+
+  fetch(
+    'https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=50&key=AIzaSyCf2Majtq2MQgIx7hgo2229KVtVHU3339w',
+    requestOptions
+  )
+    .then((response) => response.json())
+    .then((result) => {
+      const items = result.items;
+      items.forEach((item) => {
+        const video = `
+        <li class="video">
+          <img src="${item.snippet.thumbnails.medium.url}" alt="video thumbnail" class="thumbnail">
+          <div>
+            <p class="title">${item.snippet.title}</p>
+            <p class="channel">${item.snippet.channelTitle}</p>
+          </div>
+        </li>
+      `;
+        main.insertAdjacentHTML('beforeend', video);
+      });
+    })
+    .catch((error) => console.log('error', error));
 });
