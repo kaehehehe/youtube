cbc587a46a6e7a030059af8073607fcc299134d3
Kae <kaehehehe@gmail.com>
2022-02-17 00:38:26 +0900

    :sparkles: 검색 기능 구현

--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ const detailedHumbergerMenu = document.querySelector(
 const detailedNav = document.querySelector('.detailed-nav');
 const main = document.querySelector('.main');
 const videos = document.querySelector('.videos');
+const input = document.querySelector('.search__input');
+const inputBtn = document.querySelector('.search__button');
+let inputValue;
 
 defaultHumbergerMenu.addEventListener('click', () => {
   detailedNav.classList.add('show');
@@ -41,4 +44,53 @@ window.addEventListener('load', () => {
       });
     })
     .catch((error) => console.log('error', error));
+});
+
+function resetVideos() {
+  while (videos.firstChild) {
+    videos.removeChild(videos.firstChild);
+  }
+}
+
+function search() {
+  const requestOptions = {
+    method: 'GET',
+    redirect: 'follow',
+  };
+  fetch(
+    `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=${inputValue}&type=video&key=AIzaSyCf2Majtq2MQgIx7hgo2229KVtVHU3339w`,
+    requestOptions
+  )
+    .then((response) => response.json())
+    .then((result) => {
+      const items = result.items;
+      items.forEach((item) => {
+        const video = `
+        <li class="video">
+          <img src="${item.snippet.thumbnails.medium.url}" alt="video thumbnail" class="thumbnail">
+          <div>
+            <p class="title">${item.snippet.title}</p>
+            <p class="channel">${item.snippet.description}</p>
+          </div>
+        </li>
+      `;
+        videos.insertAdjacentHTML('beforeend', video);
+      });
+    })
+    .catch((error) => console.log('error', error));
+}
+
+input.addEventListener('keyup', (e) => {
+  inputValue = e.target.value;
+  if (e.key === 'Enter') {
+    if (inputValue) {
+      resetVideos();
+      search();
+    }
+  }
+});
+
+inputBtn.addEventListener('click', () => {
+  resetVideos();
+  search();
 });
